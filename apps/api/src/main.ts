459cd8e36d257655a71cc16abd704676be3044ae
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-04-15 09:24:12 +0200

    [api] init newrelic - https://vjnvisakh.medium.com/adding-new-relic-to-a-heroku-nodejs-project-a48fa5727b03

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const nr = require('newrelic');
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
