ac89143445b409973a61fcdd671d6b0230d513e9
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-04-18 22:08:42 +0200

    [api] fix failing tests

--- apps/api/src/app.controller.test.ts
+++ apps/api/src/app.controller.test.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from '~/app.controller'
 import { AppService } from '~/app.service'
+import { PrismaService } from '~/db/prisma.service'
 
 describe('AppController', () => {
   let appController: AppController
@@ -8,7 +9,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, PrismaService],
     }).compile()
 
     appController = app.get<AppController>(AppController)
@@ -16,7 +17,8 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+      const expectedResult = { data: 'pong' }
+      expect(appController.ping()).toMatchObject(expectedResult)
     })
   })
 })
