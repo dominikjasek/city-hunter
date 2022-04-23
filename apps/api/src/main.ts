4e4040ac20e98bf68ca9437d0ea0540f2449f62b
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-04-23 10:11:36 +0200

    [api] cors header for frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from '~/app.module'
 
 async function bootstrap() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const nr = require('newrelic')
+  require('newrelic')
 
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: process.env.WEB_APP_URL,
+  })
   await app.listen(process.env.PORT || 8080)
 }
 
