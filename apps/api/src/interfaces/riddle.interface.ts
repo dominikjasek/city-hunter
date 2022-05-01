64aec6ac8939654355a2637e037bc27e3ea4a3f4
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-05-01 17:45:38 +0200

    [api, web-app] share api interfaces for both apps

---
+++ apps/api/src/interfaces/riddle.interface.ts
@@ -0,0 +1,3 @@
+export interface IRiddle {
+  id: number
+}
--- apps/api/src/riddle/riddle.controller.ts
+++ apps/api/src/riddle/riddle.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpCode, HttpStatus } from '@nestjs/common'
 import { GetCurrentUserId } from '~/auth/common/decorators/index'
+import { IRiddle } from '~/interfaces/riddle.interface'
 import { RiddleService } from '~/riddle/riddle.service'
 
 @Controller('riddle')
@@ -7,7 +8,7 @@ export class RiddleController {
   constructor(private readonly riddleService: RiddleService) {}
 
   @HttpCode(HttpStatus.OK)
-  async getRiddle(@GetCurrentUserId() userId: number): Promise<any> {
+  async getRiddle(@GetCurrentUserId() userId: number): Promise<IRiddle> {
     return await this.riddleService.getRiddleForUserId(userId)
   }
 }
