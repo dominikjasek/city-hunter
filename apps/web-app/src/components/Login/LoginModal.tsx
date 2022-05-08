37ee86a44771453674c16b7c95cfb41365af31f2
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-05-08 18:28:23 +0200

    [web-app] custom colors

--- apps/web-app/src/components/Login/LoginModal.tsx
+++ apps/web-app/src/components/Login/LoginModal.tsx
@@ -18,13 +18,13 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
         <LoginGoogleButton/>
       </div>
 
-      <div className="mt-4">
+      <div className="mt-4 flex justify-end">
         <button
           type="button"
-          className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+          className="flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
           onClick={props.onCloseModal}
         >
-                    Ok!
+          <span>Zavřít</span>
         </button>
       </div>
     </Modal>
--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -4,10 +4,36 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'custom-white': '#F5DDDD',
-      'custom-blue': '#163C5B',
-      'custom-red': '#853B50',
-      'custom-orange': '#FFC914',
+      'black': '#000',
+      'white': '#fff',
+      'transparent': 'transparent',
+      'grey': {
+        '100': '#F5DDDD'
+      },
+      'orange': {
+        '900': '#FFC913',
+        '700': '#FFCF2D',
+        '500': '#FFE17B',
+        '300': '#FFEAA1',
+        '100': '#FFF2C5',
+        '50': '#FFFBEC'
+      },
+      'red': {
+        '900': '#853B50',
+        '700': '#A3566C',
+        '500': '#BD7186',
+        '300': '#DBA3B3',
+        '100': '#EEC4D0',
+        '50': '#F6DCE3'
+      },
+      'blue': {
+        '900': '#163C5B',
+        '700': '#245680',
+        '500': '#4C81AC',
+        '300': '#7CAED7',
+        '100': '#B1D5F2',
+        '50': '#DEEFFC'
+      },
     },
     extend: {},
   },
