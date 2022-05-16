c9c773b127287389dcf82241658cf4e7327e4ddf
Dominik Jašek <dominik.jasek@peckadesign.cz>
2022-05-16 11:51:33 +0200

    [web-app] clean Home component

--- apps/web-app/src/routes/Home.tsx
+++ apps/web-app/src/routes/Home.tsx
@@ -1,32 +1,11 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useAuthStore } from '~/infrastructure/auth/AuthStore'
-import { useAuthRepository } from '~/infrastructure/auth/repository/UseAuthRepository'
+import React from 'react'
 
 const Home = () => {
-  const { auth } = useAuthStore()
-  const [ data, setData ] = useState<string>('')
-  const { getProtectedRoute } = useAuthRepository()
-
-  const fetchProtectedData = async () => {
-    const val = await getProtectedRoute()
-    setData(val.data)
-  }
 
   return (
     <div>
-      <Link to={'/about'}>About</Link>
       <div>
-        <div>Hi, {auth.user?.name.firstName} {auth.user?.name.lastName}</div>
-        {
-          auth.user?.email &&
-                    <div>email: {auth.user?.email}</div>
-        }
-        <img className="m-auto" src={auth.user?.photoUrl} alt={auth.user?.name.firstName}/>
-        <button type="button" onClick={fetchProtectedData}>Fetch data</button>
-        <ul>
-          {data}
-        </ul>
+        <div>Hledej Brno!</div>
       </div>
     </div>
 
