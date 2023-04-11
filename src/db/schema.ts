b391c59617736ffaa829aa42f046d2f896e356e4
dominikjasek <dominik.jasek@customink.com>
2023-04-11 09:21:40 +0200

    init db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,17 @@
-import { mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
+import {
+  boolean,
+  datetime,
+  int,
+  json,
+  mysqlTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/mysql-core';
 import { InferModel } from 'drizzle-orm';
 
+// User
 export const users = mysqlTable('users', {
   id: varchar('id', { length: 100 }).primaryKey(),
   nickName: varchar('nick_name', { length: 40 }).notNull(),
@@ -10,3 +21,56 @@ export const users = mysqlTable('users', {
 
 export type User = InferModel<typeof users>;
 export type CreateUser = InferModel<typeof users, 'insert'>;
+
+// City
+export const cities = mysqlTable('cities', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 50 }).notNull(),
+});
+
+export type City = InferModel<typeof cities>;
+
+// Game
+export const games = mysqlTable('games', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 100 }).notNull(),
+  cityId: int('city_id').notNull(),
+});
+
+export type Game = InferModel<typeof games>;
+
+// Question
+export const questions = mysqlTable('questions', {
+  id: serial('id').primaryKey(),
+  title: varchar('title', { length: 100 }).notNull(),
+  questionDescription: text('question_description').notNull(),
+  answerDescription: text('answer_description').notNull(),
+  image: varchar('image', { length: 100 }).notNull(),
+  cityId: int('city_id'),
+  startDate: datetime('start_date'),
+  endDate: datetime('end_date'),
+  location: json('location').$type<Location>().notNull(),
+  gameId: int('game_id'),
+  demo: boolean('demo').notNull(),
+});
+
+export interface Location {
+  x: string;
+  y: string;
+}
+
+export type Question = InferModel<typeof questions>;
+
+// Answer
+export const answers = mysqlTable('answers', {
+  id: serial('id').primaryKey(),
+  location: json('location').$type<Location>().notNull(),
+  score: int('score').notNull(),
+  questionId: int('question_id').notNull(),
+  userId: varchar('user_id', { length: 100 }).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export type Answer = InferModel<typeof answers>;
+export type CreateAnswer = InferModel<typeof answers, 'insert'>;
