a7fd35d221b33aac889f44d3ebc1f8a0545dc35f
dominikjasek <dominik.jasek@customink.com>
2023-04-18 13:14:10 +0200

    dont use SSR on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,26 @@ import { NextPageWithLayout } from './_app';
 import { Button, Typography } from '@mui/material';
 import React from 'react';
 import { Loader } from '~/components/common/Loader/Loader';
+import { MessageBox } from '~/components/common/MessageBox/MessageBox';
 
 const IndexPage: NextPageWithLayout = () => {
-  const postsQuery = trpc.post.list.useQuery({
-    limit: 5,
-  });
+  const postsQuery = trpc.post.list.useQuery(
+    {
+      limit: 5,
+    },
+    {
+      trpc: {
+        ssr: false,
+      },
+    },
+  );
 
   if (postsQuery.isLoading) {
     return <Loader />;
+  }
+
+  if (postsQuery.error) {
+    return <MessageBox message={postsQuery.error.message} severity={'error'} />;
   }
 
   return (
@@ -21,7 +33,7 @@ const IndexPage: NextPageWithLayout = () => {
         <Typography fontWeight="bold">nice hochu</Typography>
       </Button>
       <br />
-      {postsQuery.data?.items.length} items fetched
+      {postsQuery.data?.items.length ?? 'nic'} items fetched
     </>
   );
 };
@@ -36,18 +48,18 @@ export default IndexPage;
  * @link https://trpc.io/docs/ssg
  */
 // export const getStaticProps = async (context: GetStaticPropsContext) => {
-//   const ssg = createProxySSGHelpers({
-//     router: appRouter,
-//     ctx: { auth: null },
-//     transformer: superjson, // optional - adds superjson serializatio
-//   });
+// const ssg = createProxySSGHelpers({
+//   router: appRouter,
+//   ctx: { auth: null },
+//   transformer: superjson, // optional - adds superjson serializatio
+// });
 //
-//   await ssg.post.list.prefetch({ limit: 5 });
+// await ssg.post.list.prefetch({ limit: 5 });
 //
-//   return {
-//     props: {
-//       trpcState: ssg.dehydrate(),
-//     },
-//     // revalidate: 100,
-//   };
+// return {
+//   props: {
+//     trpcState: ssg.dehydrate(),
+//   },
+//   // revalidate: 100,
+// };
 // };
