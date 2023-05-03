786f01484262bc5c1f0223da0cb5d37bee60db77
dominikjasek <dominik.jasek@customink.com>
2023-05-03 17:15:16 +0200

    fix SSR date time formatting problem - bug description: - on server it renders time in UTC - on client it renders time in CEST (summer time in Prague)

--- src/utils/formatter/dateFormatter.ts
+++ src/utils/formatter/dateFormatter.ts
@@ -1,13 +1,15 @@
-import { format } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+
+const DEFAULT_TIMEZONE = 'Europe/Prague';
 
 export const formatDate = (date: Date) => {
-  return format(date, 'd.M.yyyy');
+  return formatInTimeZone(date, DEFAULT_TIMEZONE, 'd.M.yyyy');
 };
 
 export const formatDateTime = (date: Date) => {
-  return format(date, 'd.M.yyyy HH:mm');
+  return formatInTimeZone(date, DEFAULT_TIMEZONE, 'd.M.yyyy HH:mm');
 };
 
 export const formatTime = (date: Date) => {
-  return format(date, 'HH:mm');
+  return formatInTimeZone(date, DEFAULT_TIMEZONE, 'HH:mm');
 };
