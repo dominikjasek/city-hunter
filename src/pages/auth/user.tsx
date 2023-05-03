890c5790ff22f29a844c02720dbdf63ff4c22bb9
dominikjasek <dominik.jasek@customink.com>
2023-05-03 23:47:06 +0200

    invalidate nickname query on submit

--- src/pages/auth/user.tsx
+++ src/pages/auth/user.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { trpc } from '~/utils/trpc';
 import { Loader } from '~/components/common/Loader/Loader';
+import { MessageBox } from '~/components/common/MessageBox/MessageBox';
 
 const userProfileValidationSchema = z.object({
   nickName: z.string().min(3, 'Přezdívka musí mít alespoň 3 znaky').max(20, 'Přezdívka může mít maximálně 20 znaků'),
@@ -29,10 +30,17 @@ const UserPage: NextPage = () => {
     }
   }, [currentNickName]);
 
-  const { mutateAsync, isLoading: isSubmitting } = trpc.auth.updateNickName.useMutation();
+  const {
+    mutateAsync,
+    isLoading: isSubmitting,
+    isSuccess: isNickNameSuccessfullyChanged,
+  } = trpc.auth.updateNickName.useMutation();
+
+  const utils = trpc.useContext();
 
   const onFormSubmit = async (data: UserProfileValidationSchema) => {
     await mutateAsync(data);
+    await utils.invalidate(undefined, { queryKey: ['auth.getNickName'] });
   };
 
   if (isNickNameLoading) {
@@ -44,24 +52,37 @@ const UserPage: NextPage = () => {
       <Typography variant="h5" my={3}>
         Váš účet
       </Typography>
-      <form onSubmit={handleSubmit(onFormSubmit)}>
-        <Stack direction={'column'} maxWidth={{ xs: 300, md: 500 }}>
-          <TextField
-            fullWidth
-            label={'Přezdívka zobrazená v žebříčku'}
-            variant="filled"
-            color={'secondary'}
-            required
-            error={!!errors['nickName']}
-            helperText={errors['nickName'] ? errors['nickName'].message : ''}
-            {...register('nickName')}
-          />
+      {isNickNameSuccessfullyChanged ? (
+        <MessageBox
+          message={'Přezdívka byla úspěšně změněna. Změna přezdívky se v žebříčkách projeví do 1 minuty.'}
+          type={'success'}
+        />
+      ) : (
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+          <Stack direction={'column'} maxWidth={{ xs: 300, md: 500 }}>
+            <TextField
+              fullWidth
+              label={'Přezdívka zobrazená v žebříčku'}
+              variant="filled"
+              color={'secondary'}
+              required
+              error={!!errors['nickName']}
+              helperText={errors['nickName'] ? errors['nickName'].message : ''}
+              {...register('nickName')}
+            />
 
-          <Button variant={'contained'} color={'secondary'} type="submit" sx={{ my: 4, px: 6 }} disabled={isSubmitting}>
-            Uložit
-          </Button>
-        </Stack>
-      </form>
+            <Button
+              variant={'contained'}
+              color={'secondary'}
+              type="submit"
+              sx={{ my: 4, px: 6 }}
+              disabled={isSubmitting}
+            >
+              Uložit
+            </Button>
+          </Stack>
+        </form>
+      )}
     </Box>
   );
 };
