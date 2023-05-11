cfe1e454400e854a096071bf0b47803223c4cec2
dominikjasek <dominik.jasek@customink.com>
2023-05-11 23:49:17 +0200

    LoadingButton on Question Task, it is necessary to specify backgroundColor manually when position is relative, LOL

--- src/components/Question/QuestionTask.tsx
+++ src/components/Question/QuestionTask.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
 import { MapPicker } from '~/components/MapPicker/MapPicker';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
 import Image from 'next/image';
 import { MapLocation } from '~/components/MapPicker/types';
 import { LightBox } from '~/components/LightBox/LightBox';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 interface QuestionTaskProps {
   city: {
@@ -21,6 +22,8 @@ export const QuestionTask: FC<QuestionTaskProps> = (props) => {
   const [isLightboxOpen, setIsLightboxOpen] = useState(false);
   const [point, setPoint] = useState<MapLocation | null>(null);
 
+  const theme = useTheme();
+
   return (
     <>
       <LightBox isOpen={isLightboxOpen} imagesUrl={[props.questionImageUrl]} onClose={() => setIsLightboxOpen(false)} />
@@ -30,13 +33,11 @@ export const QuestionTask: FC<QuestionTaskProps> = (props) => {
           px: { xs: 0, sm: 1, md: 2 },
         }}
       >
-        <Stack direction={'row'} justifyContent={'center'} alignItems={'center'} gap={4}>
+        <Stack mb={2} direction={'column'} alignItems={'center'} gap={2}>
           <Box>
             <Typography variant={'h6'}>{props.title}</Typography>
             <Typography sx={{ mb: 2 }}>{props.questionDescription}</Typography>
           </Box>
-        </Stack>
-        <Stack mb={2} direction={'column'} alignItems={'center'} gap={2}>
           <Box
             sx={{
               minWidth: 300,
@@ -64,25 +65,28 @@ export const QuestionTask: FC<QuestionTaskProps> = (props) => {
               onClick={setPoint}
             />
           </Box>
+          <LoadingButton
+            variant={'contained'}
+            color={'secondary'}
+            size={'large'}
+            loading={props.isSubmitting}
+            sx={{
+              width: 'calc(100% - 20px)',
+              py: 2,
+              mx: 'auto',
+              position: 'relative',
+              top: -85,
+              zIndex: 1000,
+              display: point ? 'inherit' : 'none',
+              '&.Mui-disabled': {
+                backgroundColor: theme.palette.grey.A400,
+              },
+            }}
+            onClick={() => props.onSubmit(point!)}
+          >
+            Potvrdit
+          </LoadingButton>
         </Stack>
-        <Button
-          variant={'contained'}
-          color={'secondary'}
-          size={'large'}
-          disabled={!point || props.isSubmitting}
-          sx={{
-            width: 'calc(100% - 20px)',
-            py: 2,
-            mx: 'auto',
-            position: 'relative',
-            top: -85,
-            zIndex: 1,
-            display: point ? 'block' : 'none',
-          }}
-          onClick={() => props.onSubmit(point!)}
-        >
-          Potvrdit
-        </Button>
       </Box>
     </>
   );
