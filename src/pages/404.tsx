9bf56a27299dc6d724abbbf32d228f78113b568e
dominikjasek <dominik.jasek@customink.com>
2023-05-12 14:18:57 +0200

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+const Custom404: NextPage = () => {
+  return <Typography variant={'h6'}>404 - Ať hledáme jak hledáme, tady nic nenajdeme.</Typography>;
+};
+
+export default Custom404;
