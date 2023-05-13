2f91f415aa5e20fcdd909ae67aa31b877fa82493
dominikjasek <dominik.jasek@customink.com>
2023-05-13 21:31:20 +0200

    💅styling of Round Links

--- src/components/ranking/TournamentRoundLinks.tsx
+++ src/components/ranking/TournamentRoundLinks.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
-import { Box, Stack, styled, Typography } from '@mui/material';
+import { Box, Stack, styled, Typography, useTheme } from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const RoundOrderLink = styled(Typography)(({ theme }) => ({
+const FONT_SIZE_MULTIPLIER = 1.4;
+
+const RoundOrderLink = styled(Typography)<{ isActive: boolean }>(({ theme, isActive }) => ({
   color: theme.palette.secondary.main,
+  borderColor: theme.palette.secondary.main,
+  borderBottom: isActive ? 'solid 2px' : 0,
+  fontWeight: isActive ? 'bold' : 'normal',
+  fontSize: theme.typography.fontSize * FONT_SIZE_MULTIPLIER,
   '&:hover': {
     color: theme.palette.secondary.light,
     cursor: 'pointer',
@@ -23,38 +29,27 @@ interface TournamentRoundLinksProps {
 
 export const TournamentRoundLinks: FC<TournamentRoundLinksProps> = ({ tournamentId, tournamentQuestions }) => {
   const router = useRouter();
+  const theme = useTheme();
 
   const now = new Date();
 
   return (
     <Stack direction={'row'} my={3} gap={2} justifyContent={{ xs: 'start', lg: 'center' }} sx={{ overflowX: 'auto' }}>
-      <RoundOrderLink color={'white'} sx={{ color: 'white', '&:hover': { color: 'white', cursor: 'text' } }}>
-        Kolo:
-      </RoundOrderLink>
+      <Typography fontSize={theme.typography.fontSize * FONT_SIZE_MULTIPLIER}>Kolo:</Typography>
       <Link href={`/ranking/${tournamentId}`} className={'no-style'}>
-        <RoundOrderLink
-          mr={2}
-          sx={{
-            textDecoration: router.query.roundOrder === undefined ? 'underline' : 'none',
-            textUnderlineOffset: 6,
-          }}
-        >
+        <RoundOrderLink isActive={router.query.roundOrder === undefined} mr={2}>
           CELKOVĚ
         </RoundOrderLink>
       </Link>
       {tournamentQuestions.map((question) => (
         <Box key={question.id} sx={{ display: 'inline' }}>
           {now.getTime() < question.endDate.getTime() ? (
-            <Typography sx={{ cursor: 'not-allowed' }}>{question.roundOrder}</Typography>
+            <Typography sx={{ cursor: 'not-allowed', fontSize: theme.typography.fontSize * FONT_SIZE_MULTIPLIER }}>
+              {question.roundOrder}
+            </Typography>
           ) : (
             <Link href={`/ranking/${tournamentId}/${question.roundOrder}`} className={'no-style'}>
-              <RoundOrderLink
-                sx={{
-                  textDecoration:
-                    router.asPath === `/ranking/${tournamentId}/${question.roundOrder}` ? 'underline' : 'none',
-                  textUnderlineOffset: 6,
-                }}
-              >
+              <RoundOrderLink isActive={router.asPath === `/ranking/${tournamentId}/${question.roundOrder}`}>
                 {question.roundOrder}
               </RoundOrderLink>
             </Link>
