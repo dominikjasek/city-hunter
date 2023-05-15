1d2301842d5766d2a349b748ef3d20eae021f457
dominikjasek <dominik.jasek@customink.com>
2023-05-15 15:04:54 +0200

    update distance coefficient

--- src/utils/score/constants.ts
+++ src/utils/score/constants.ts
@@ -1,8 +1,8 @@
-// see demo here: https://www.desmos.com/calculator/yifremdsy9
+// see demo here: https://www.desmos.com/calculator/gnpinecbni
 // t stands for time in minutes
 // d stands for distance in meters
 
 export const T_1 = 1.01; // time start ignore coefficient
 export const T_2 = 0.2; // time penalty coefficient
 export const D_1 = 115; // distance proximity ignore coefficient
-export const D_2 = 1; // distance coefficient
+export const D_2 = 0.75; // distance coefficient
