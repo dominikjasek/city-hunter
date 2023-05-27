b56feede23eef6eb5185569d948943071bfed520
dominikjasek <dominik.jasek@customink.com>
2023-05-27 16:56:17 +0200

    generate sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cityhunter.cz',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: ['/admin', '/admin/**'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: ['/admin', '/admin/**', '/auth/login-receiver', '/unauthorized'],
+      },
+    ],
+  },
+};
