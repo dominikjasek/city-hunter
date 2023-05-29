180f19c886f2ced665045e3d5de7c5fd61102d9e
dominikjasek <dominik.jasek@customink.com>
2023-05-29 22:21:21 +0200

    remove `sso-callback` route, not necessary?

--- src/pages/sso-callback.tsx
+++
@@ -1,7 +0,0 @@
-import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
-
-export const SsoCallback = () => {
-  return <AuthenticateWithRedirectCallback />;
-};
-
-export default SsoCallback;
