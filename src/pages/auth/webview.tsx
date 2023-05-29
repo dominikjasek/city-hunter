9250d53f004880c73e483fb40527917f86de25a3
dominikjasek <dominik.jasek@customink.com>
2023-05-29 08:43:51 +0200

    redirect from webview to login on mobile browser

--- src/pages/auth/webview.tsx
+++ src/pages/auth/webview.tsx
@@ -1,11 +1,23 @@
 import { NextPage } from 'next';
 import { MessageBox } from '~/components/common/MessageBox/MessageBox';
+import isWebview from 'is-ua-webview';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Webview: NextPage = () => {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    const isWebView = isWebview(window.navigator.userAgent);
+    if (!isWebView) {
+      push('/auth/login');
+    }
+  });
+
   return (
     <MessageBox
       message={
-        'Používáte mobilní WebView prohlížeč. Pro přihlášení je nutné, abyste otevřeli tuto stránku ve vašem výchozím'
+        'Používáte mobilní WebView prohlížeč. Z bezpečnostních důvodů je pro přihlášení potřebné, abyste otevřeli tuto stránku ve vašem výchozím prohlížeči.'
       }
       type={'warning'}
     />
